b8aad582d60bbdb3dcf0c61ee622d3f838cd2656
Fat Myxtap <fatmyxtap@yandex.ru>
2021-05-12 12:47:06 +0000

    Updated security config to support both auth types.

--- src/main/kotlin/com/kastbin/configuration/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/kastbin/configuration/WebSecurityConfiguration.kt
@@ -3,27 +3,45 @@ package com.kastbin.configuration
 import com.kastbin.service.OAuth2UserServiceImpl
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
+import javax.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+
+import javax.servlet.http.HttpServletResponse
+
+import javax.servlet.ServletException
+
+import java.io.IOException
+
+import javax.servlet.FilterChain
+
+import javax.servlet.ServletResponse
+
+import javax.servlet.ServletRequest
+
+import org.springframework.web.filter.GenericFilterBean
 
 @EnableWebSecurity
 class WebSecurityConfiguration {
+
     @Configuration
-    @Order(1)
-    class WebSecurityAuth(
+    class WebSecurityOAuth(
         private val userDetailsService: UserDetailsService,
         private val oauth: OAuth2UserServiceImpl,
         private val bcrypt: Bcrypt
     ) : WebSecurityConfigurerAdapter() {
+
         override fun configure(http: HttpSecurity) {
             http
                 .authorizeRequests()
-                .antMatchers("/", "/past", "/signup")
-                .permitAll().anyRequest()
-                .authenticated().and()
+                .mvcMatchers("/", "/past", "/signup").permitAll()
+                .mvcMatchers("/oauth/**").authenticated()
+                .and()
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(oauth)
@@ -37,17 +55,19 @@ class WebSecurityConfiguration {
     }
 
     @Configuration
-    class WebBasicConfig(
+    @Order(0)
+    class WebSecurityBasic(
         private val userDetailsService: UserDetailsService,
         private val bcrypt: Bcrypt
     ) : WebSecurityConfigurerAdapter() {
 
         override fun configure(http: HttpSecurity) {
             http
+                .requestMatcher(BasicRequestMatcher())
                 .authorizeRequests()
-                .antMatchers("/", "/past", "/signup", "/Oauth")
-                .permitAll().anyRequest()
-                .authenticated().and()
+                .mvcMatchers("/", "/past", "/signup").permitAll()
+                .mvcMatchers("/basic/**").authenticated()
+                .and()
                 .httpBasic()
         }
 
@@ -55,6 +75,13 @@ class WebSecurityConfiguration {
             auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(bcrypt.hash())
+        }
+
+        internal class BasicRequestMatcher : RequestMatcher {
+            override fun matches(request: HttpServletRequest): Boolean {
+                val auth = request.getHeader("Authorization")
+                return auth != null && auth.startsWith("Basic")
+            }
         }
 
     }
